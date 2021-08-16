69a57de0fa8f0bf537f248bb0b8c20e9f6774d03
Pedro Antonio Rodrigues <pedro_arxp@hotmail.com>
2021-08-16 11:18:22 -0300

    Add files via upload

---
+++ indexx/app.js
@@ -0,0 +1,7 @@
+function calcular (){
+    let n1 = parseFloat (document.getElementsByTagName('input')[0].value);
+    let n2 = parseFloat (document.getElementsByTagName('input')[1].value);
+document.getElementsByTagName ('output')[0].innerText = n1 + n2;
+
+}
+
